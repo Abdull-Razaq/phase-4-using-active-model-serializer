1278801ed2c109899a5634c181f00c3699150c6a
Abdirizak Mohamed <abdirizakm123@gmail.com>
2023-03-24 07:56:37 +0300

    Completed assignment

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -3,12 +3,22 @@ class MoviesController < ApplicationController
 
   def index
     movies = Movie.all
-    render json: movies
+    render json: movies 
   end
 
   def show
     movie = Movie.find(params[:id])
     render json: movie
+  end
+
+  def summary
+    movie = Movie.find(params[:id])
+    render json: movie, serializer: MovieSummarySerializer
+  end
+
+  def summaries
+    movies = Movie.all
+    render json: movies, eacH_serializer: MovieSummarySerializer
   end
 
   private
